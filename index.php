65dac49fbb4fc5892ef10ca977d97d841b396788
wteuling <wteuling@bcit.ca>
2022-01-24 16:36:54 -0800

    author change to william

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="english language">
-    <meta name="author" content="Wim Teuling">
+    <meta name="author" content="William Teuling">
 
     <title>Our website - home page</title>
 
